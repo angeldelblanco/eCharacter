e5157389a71198b6f3df954ba3ab39965fb493c2
Alejandro Muñoz del Rey <alejandromrey@gmail.com>
2013-03-29 14:05:43 +0100

    Generadas nuevas clase en el paquete data.model como consecuencia de los cambios en la xsd

--- src/data/model/ObjectFactory.java
+++ src/data/model/ObjectFactory.java
@@ -1,12 +1,12 @@
 //
-// Este archivo ha sido generado por la arquitectura JavaTM para la implantación de la referencia de enlace (JAXB) XML v2.2.6 
+// Este archivo ha sido generado por la arquitectura JavaTM para la implantaciï¿½n de la referencia de enlace (JAXB) XML v2.2.6 
 // Visite <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
-// Todas las modificaciones realizadas en este archivo se perderán si se vuelve a compilar el esquema de origen. 
+// Todas las modificaciones realizadas en este archivo se perderï¿½n si se vuelve a compilar el esquema de origen. 
 // Generado el: PM.03.29 a las 12:58:59 PM CET 
 //
 
 
-package model;
+package data.model;
 
 import javax.xml.bind.annotation.XmlRegistry;
 
